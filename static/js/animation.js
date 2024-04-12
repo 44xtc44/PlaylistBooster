3e36028cf0f8b559cce055451a3822f201a399bf
4xtc <44xtc44@gmail.com>
2024-04-12 13:34:37 +0200

    upd docstring

--- static/js/animation.js
+++ static/js/animation.js
@@ -3,10 +3,15 @@
 
 var animationFrameCount = 0;
 
+/**
+ * All animations will get called here.
+ * Runs on Browser refresh rate.
+ */
 function animationMain() {
-    let idx = switchAnalyzer.analyzerListIdx;
-    switchAnalyzer.analyzerList[idx]();
-    
-    /* requestAnimationFrame; only one in an app. */
-    animationFrameCount = requestAnimationFrame(animationMain);
-  }
+
+  let idx = switchAnalyzer.analyzerListIdx;
+  switchAnalyzer.analyzerList[idx]();
+
+  /* requestAnimationFrame; only one in an app. */
+  animationFrameCount = requestAnimationFrame(animationMain);
+}
